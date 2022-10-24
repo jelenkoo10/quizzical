03753f948cae5363fed755cd8a3854aeea200c59
jelenkoo10 <veljkojelenkovic00@gmail.com>
2022-10-25 00:00:21 +0200

    Updating HashRouter

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom"
 import App from "./App"
 import { HashRouter } from "react-router-dom"
 
-ReactDOM.render(<HashRouter>	
+ReactDOM.render(<HashRouter basename="/">	
 			        <App />
     		    </HashRouter>, document.getElementById("root"))
