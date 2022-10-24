d3d034b160386fc9339c31a35659cedc199b6520
jelenkoo10 <veljkojelenkovic00@gmail.com>
2022-10-24 23:55:47 +0200

    Importing HashRouter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
-import { BrowserRouter as Router } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 
-ReactDOM.render(<Router>	
+ReactDOM.render(<HashRouter>	
 			        <App />
-    		    </Router>, document.getElementById("root"))
+    		    </HashRouter>, document.getElementById("root"))
